edd09db4f47d10a6941f8ed62f63e3c569b2dfab
SiHecate <blrumutcan64@gmail.com>
2023-11-21 23:39:57 +0300

    Product ve Table'a Update, Delete metotları eklendi

--- controller/product.go
+++ controller/product.go
@@ -42,6 +42,48 @@ func AddProduct(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{"message": "Product created successfully", "product": newProduct})
 }
 
+func UpdateProduct(c *fiber.Ctx) error {
+	var productData struct {
+		ProductName     string  `json:"product_name"`
+		ProductCategory string  `json:"product_category"`
+		ProductPrice    float64 `json:"product_price"`
+	}
+
+	if err := c.BodyParser(&productData); err != nil {
+		return c.Status(422).JSON(fiber.Map{"error": "Invalid request data: " + err.Error()})
+	}
+
+	var existingProduct models.Product
+	if err := database.Conn.Where("product_name = ?", productData.ProductName).First(&existingProduct).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Database error"})
+	}
+
+	// Ürünlere yeni özellikler girildikçe eklentiler gerekebilir.
+	existingProduct.ProductCategory = productData.ProductCategory
+	existingProduct.ProductPrice = productData.ProductPrice
+
+	if err := database.Conn.Save(&existingProduct).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Product update failed"})
+	}
+
+	return c.JSON(fiber.Map{"message": "Product updated successfully", "product": existingProduct})
+}
+
+func DeletProduct(c *fiber.Ctx) error {
+	ProductName := c.Params("product_name")
+
+	var existingProduct models.Product
+	if err := database.Conn.Where("product_name", ProductName).First(&existingProduct).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"error": "Product not found"})
+	}
+
+	if err := database.Conn.Delete(&existingProduct).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Product deletion failed"})
+	}
+
+	return c.JSON(fiber.Map{"message": "Product deleted successfully"})
+}
+
 func ListProducts(c *fiber.Ctx) error {
 	var existingProducts []models.Product
 	if err := database.Conn.Find(&existingProducts).Error; err != nil {
--- controller/table.go
+++ controller/table.go
@@ -38,7 +38,51 @@ func AddTable(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(fiber.Map{"message": "Table created successfully", "table": newTable})
+}
 
+func UpdateTable(c *fiber.Ctx) error {
+	var tableData struct {
+		TableNo       string `json:"tableno"`
+		TablePassword string `json:"table_password"`
+		Capacity      int    `json:"capacity"`
+	}
+
+	if err := c.BodyParser(&tableData); err != nil {
+		return c.Status(422).JSON(fiber.Map{"error": "Invalid request data: " + err.Error()})
+	}
+
+	var existingTable models.Table
+	if err := database.Conn.Where("table_no", tableData.TableNo).First(&existingTable).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Database error"})
+	}
+
+	if existingTable.ID == 0 {
+		return c.Status(404).JSON(fiber.Map{"error": "Table not found"})
+	}
+
+	// Masalara yeni özellikler girildikçe eklentiler gerekebilir.
+	existingTable.Capacity = tableData.Capacity
+
+	if err := database.Conn.Save(&existingTable).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Table update failed"})
+	}
+
+	return c.JSON(fiber.Map{"message": "Table updated successfully", "table": existingTable})
+}
+
+func DeleteTable(c *fiber.Ctx) error {
+	tableNo := c.Params("tableNo")
+
+	var existingTable models.Table
+	if err := database.Conn.Where("table_no", tableNo).First(&existingTable).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"error": "Table not found"})
+	}
+
+	if err := database.Conn.Delete(&existingTable).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Table deletion failed"})
+	}
+
+	return c.JSON(fiber.Map{"message": "Table deleted successfully"})
 }
 
 func ListTables(c *fiber.Ctx) error {
